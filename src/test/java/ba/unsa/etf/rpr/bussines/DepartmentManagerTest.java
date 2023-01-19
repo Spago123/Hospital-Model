9f8267e141e6e965ce2bd9534f1536d3ad3ccdc2
Spago123 <hspago1@etf.unsa.ba>
2023-01-19 10:35:25 +0100

    New things art one

--- src/test/java/ba/unsa/etf/rpr/bussines/DepartmentManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/bussines/DepartmentManagerTest.java
@@ -4,15 +4,18 @@ import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.dao.DepartmentDaoSQLImpl;
 import ba.unsa.etf.rpr.domain.Department;
 import ba.unsa.etf.rpr.exceptions.HospitalException;
-import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import static org.mockito.Mockito.when;
 
 public class DepartmentManagerTest {
 
@@ -29,7 +32,7 @@ public class DepartmentManagerTest {
         department = new Department(40, "Lobotomija");
 
         departmentList = new ArrayList<>();
-        departmentList.addAll(Arrays.asList(new Department(40, "Lobotomija"), new Department(41, "Kardiologija")
+        departmentList.addAll(Arrays.asList(new Department(40, "Novi odjel"), new Department(41, "Kardiologija")
                 , new Department(42, "Ortopedija")));
 
     }
@@ -70,16 +73,29 @@ public class DepartmentManagerTest {
 
     @Test
     public void addDepartment() throws HospitalException {
-        Department newDepartment = new Department(40, "Novi odjel");
-        Mockito.doCallRealMethod().when(departmentManager).add(newDepartment);
-        departmentManager.add(newDepartment);
+        MockedStatic<DaoFactory> daoFactoryMockedStatic = Mockito.mockStatic(DaoFactory.class);
+        daoFactoryMockedStatic.when(DaoFactory::departmentDao).thenReturn(departmentDaoSQLMock);
+        when(DaoFactory.departmentDao().getAll()).thenReturn(departmentList);
+        when(departmentManager.getAll()).thenReturn(departmentList);
+
+        //when(DaoFactory.departmentDao().add(department)).thenAnswer(() ->{departmentList.add(department);});
+
+
+        department = new Department(50, "Stari odjel");
+       // Mockito.doCallRealMethod().when(daoFactoryMockedStatic).add(department);
+        Mockito.doCallRealMethod().when(departmentManager).add(department);
+        departmentManager.add(department);
 
         Assertions.assertTrue(true);
-        Mockito.verify(departmentManager).add(newDepartment);
+      //  daoFactoryMockedStatic.verify(DaoFactory::departmentDao);
+        Mockito.verify(departmentManager).add(department);
+        daoFactoryMockedStatic.close();
+
     }
 
-    @Test
+    @Disabled
     public void deleteDepartment() throws HospitalException {
+        when(departmentManager.getAll()).thenReturn(departmentList);
         Department deleteDepartment = new Department(40, "Novi odjel");
         Mockito.doCallRealMethod().when(departmentManager).delete(deleteDepartment.getId());
         departmentManager.delete(deleteDepartment.getId());
@@ -90,12 +106,15 @@ public class DepartmentManagerTest {
 
     @Test
     void addAlreadyExisting() throws HospitalException {
-        Department newDepartment = new Department(40, "Novi odjel");
-        Mockito.doCallRealMethod().when(departmentManager).add(newDepartment);
+        when(departmentManager.getAll()).thenReturn(departmentList);
 
-        HospitalException exception = Assertions.assertThrows(HospitalException.class, () -> departmentManager.add(newDepartment),
-                "Department already exists");
+        department = new Department(40, "Novi odjel");
+        Mockito.doCallRealMethod().when(departmentManager).add(department);
 
-        Assert.assertEquals("Department already exists", exception.getMessage());
+        HospitalException exception = Assertions.assertThrows(HospitalException.class, () -> {departmentManager.add(department);});
+
+        Assertions.assertEquals(department.getName() + " department already exists!", exception.getMessage());
+
+        Mockito.verify(departmentManager).add(department);
     }
 }
