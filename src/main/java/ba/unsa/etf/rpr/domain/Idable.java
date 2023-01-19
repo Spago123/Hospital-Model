3de34f73c556aa01eebc302743e7db0a60809cc1
Spago123 <hspago1@etf.unsa.ba>
2023-01-19 10:57:55 +0100

    JavaDoc Idable

--- src/main/java/ba/unsa/etf/rpr/domain/Idable.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Idable.java
@@ -1,5 +1,9 @@
 package ba.unsa.etf.rpr.domain;
 
+/**
+ * Idable is an interface that has to be implemented be every class
+ * that represents an entity in the Data Base
+ */
 public interface Idable {
 
     void setId(int id);
