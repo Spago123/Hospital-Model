892e679b7df00fcea28b35cc6884a3a14be62a21
Spago123 <hspago1@etf.unsa.ba>
2023-01-19 11:31:41 +0100

    JavaDoc DepartmentDao

--- src/main/java/ba/unsa/etf/rpr/dao/DepartmentDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DepartmentDao.java
@@ -2,5 +2,9 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Department;
 
+/**
+ * DepartmentDao is an interface that has to be implemented
+ * by the DaoSqlImpl that works with the Department table in the database
+ */
 public interface DepartmentDao extends Dao<Department>{
 }
