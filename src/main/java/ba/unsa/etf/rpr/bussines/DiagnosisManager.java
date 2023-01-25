b18908eff3ecb7e5a3b1dd9a6f588f85504774a8
Spago123 <hspago1@etf.unsa.ba>
2023-01-25 21:11:56 +0100

    JavaDoc DiagnosisManager

--- src/main/java/ba/unsa/etf/rpr/bussines/DiagnosisManager.java
+++ src/main/java/ba/unsa/etf/rpr/bussines/DiagnosisManager.java
@@ -8,32 +8,72 @@ import ba.unsa.etf.rpr.exceptions.HospitalException;
 
 import java.util.List;
 
+/**
+ * Diagnosis manager is a class that models our work with the Histories table in the database
+ */
 public class DiagnosisManager implements Manager<History>{
-
+    /**
+     * Method that lets us return all histories that belong to the same patient
+     * @param patient whose histories we are searching
+     * @return list of histories
+     */
     public List<History> getByPatient(Patient patient){
         return DaoFactory.historyDao().searchByPatient(patient);
     }
 
+    /**
+     * Method that lets us return all histories that were given by the same doctor
+     * @param doctor whose histories we are searching
+     * @return list of histories
+     */
     public List<History> getByDoctor(Doctor doctor){
         return DaoFactory.historyDao().searchByDoctor(doctor);
     }
 
+    /**
+     * Method that lets us add a history to the database
+     * @param item we want to add
+     * @throws HospitalException
+     */
     public void add(History item) throws HospitalException {
         DaoFactory.historyDao().add(item);
     }
 
+    /**
+     * Method that return the history from the database that has the provided id
+     * @param id we search
+     * @return history with the given id
+     * @throws HospitalException
+     */
     public History getById(int id) throws HospitalException {
         return DaoFactory.historyDao().getById(id);
     }
+
+    /**
+     * Method that return all histories from the database
+     * @return list of histories
+     * @throws HospitalException
+     */
     public List<History> getAll() throws HospitalException {
         return DaoFactory.historyDao().getAll();
     }
 
+    /**
+     * Method that lets us delete a history from the database that has the
+     * provided id
+     * @param id of the element we want to delete
+     * @throws HospitalException
+     */
     @Override
     public void delete(int id) throws HospitalException {
         DaoFactory.historyDao().delete(id);
     }
 
+    /**
+     * Method that lets us update a history in the database
+     * @param item we want to update
+     * @throws HospitalException
+     */
     @Override
     public void update(History item) throws HospitalException {
         DaoFactory.historyDao().update(item);
