8aac1e69f0877eadaf8257e1f9390735119f1bf2
Spago123 <hspago1@etf.unsa.ba>
2023-01-25 21:33:15 +0100

    JavaDoc OneButtonCellFactory

--- src/main/java/ba/unsa/etf/rpr/controllers/components/OneButtonCellFactory.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/components/OneButtonCellFactory.java
@@ -8,19 +8,29 @@ import javafx.scene.control.TableColumn;
 import javafx.util.Callback;
 
 
-
-
+/**
+ * Class that lets you make a button inside a table
+ * @param <T>
+ */
 public class OneButtonCellFactory<T> implements Callback<TableColumn<T, T>, TableCell<T, T>> {
     private final EventHandler<ActionEvent> buttonOne;
     private final String name;
 
-
+    /**
+     * Constructor
+     * @param buttonOne event to call when button is pressed
+     * @param name to display on the button
+     */
     public OneButtonCellFactory(EventHandler<ActionEvent> buttonOne, String name) {
         this.buttonOne = buttonOne;
         this.name = name;
     }
 
-
+    /**
+     *
+     * @param ttTableColumn
+     * @return
+     */
     @Override
     public TableCell<T, T> call(TableColumn<T, T> ttTableColumn) {
         return new OneButtonTableCell<>(buttonOne, name);
