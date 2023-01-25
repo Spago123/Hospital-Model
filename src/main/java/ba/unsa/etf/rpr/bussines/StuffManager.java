bd076cf8141fca39f7b99ea9a3cb6f5b34dc3a9d
Spago123 <hspago1@etf.unsa.ba>
2023-01-25 21:27:30 +0100

    JavaDoc StuffManager finished

--- src/main/java/ba/unsa/etf/rpr/bussines/StuffManager.java
+++ src/main/java/ba/unsa/etf/rpr/bussines/StuffManager.java
@@ -1,7 +1,14 @@
 package ba.unsa.etf.rpr.bussines;
 
+/**
+ * Stuff manger is a manager class which task is to verify different things
+ */
 public class StuffManager {
-
+    /**
+     * Method that verifies if the password is valid
+     * @param pass
+     * @return boolean
+     */
     public static boolean verifyPassword(String pass){
         if(pass == null || pass.length()<7 || pass.length()>15){
             return false;
