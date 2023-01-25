49655a1f4cbf6bb8ff859051baf9ec48f1eab646
Spago123 <hspago1@etf.unsa.ba>
2023-01-25 20:57:30 +0100

    JavaDoc Manager Finished

--- src/main/java/ba/unsa/etf/rpr/bussines/Manager.java
+++ src/main/java/ba/unsa/etf/rpr/bussines/Manager.java
@@ -19,12 +19,32 @@ public interface Manager<Type extends Idable> {
      */
     Type getById(int id) throws HospitalException;
 
+    /**
+     * Method that return every element from a table in a databse
+     * @return list of elements
+     * @throws HospitalException
+     */
     List<Type> getAll() throws HospitalException;
 
+    /**
+     * Method which deletes an element from the database
+     * @param id of the element we want to delete
+     * @throws HospitalException
+     */
     void delete(int id) throws HospitalException;
 
+    /**
+     * Method which updates an item in the database
+     * @param item we want to update
+     * @throws HospitalException
+     */
     void update(Type item) throws HospitalException;
 
+    /**
+     * Method that adds an item to the database
+     * @param item we want to add
+     * @throws HospitalException
+     */
     void add(Type item) throws HospitalException;
 
 }
