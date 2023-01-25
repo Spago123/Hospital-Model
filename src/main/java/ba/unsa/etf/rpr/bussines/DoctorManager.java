321ac4ea71c82e103dba4c8b1d4795b6136b9716
Spago123 <hspago1@etf.unsa.ba>
2023-01-25 21:16:53 +0100

    JavaDoc DoctorManager

--- src/main/java/ba/unsa/etf/rpr/bussines/DoctorManager.java
+++ src/main/java/ba/unsa/etf/rpr/bussines/DoctorManager.java
@@ -6,12 +6,25 @@ import ba.unsa.etf.rpr.exceptions.HospitalException;
 
 import java.util.List;
 
+/**
+ * DoctorManager is a class that models our work with the Doctor table in the database
+ */
 public class DoctorManager implements Manager<Doctor>{
+    /**
+     * Method that lets us update an doctor in the database
+     * @param item we want to update
+     * @throws HospitalException
+     */
     @Override
     public void update(Doctor item) throws HospitalException {
         DaoFactory.doctorDao().update(item);
     }
 
+    /**
+     * Method that lets us add a doctor to the Doctors table
+     * @param item we want to add
+     * @throws HospitalException
+     */
     @Override
     public void add(Doctor item) throws HospitalException {
         for(Doctor doctor : getAll()){
@@ -21,19 +34,42 @@ public class DoctorManager implements Manager<Doctor>{
         DaoFactory.doctorDao().add(item);
     }
 
+    /**
+     * Method that lets us search a doctor by name and password
+     * @param name of the doctor
+     * @param pass of the doctor
+     * @return doctor
+     */
     public List<Doctor> getByNameAndPass(String name, String pass){
         return DaoFactory.doctorDao().searchByNameAndPassword(name, pass);
     }
 
+    /**
+     * Method that lets us get a doctor from the database
+     * that has the provided id
+     * @param id we search
+     * @return doctor with the given id
+     * @throws HospitalException
+     */
     public Doctor getById(int id) throws HospitalException {
         return DaoFactory.doctorDao().getById(id);
     }
 
+    /**
+     * Method that lets us get all doctors from the doctor table
+     * @return list of doctors
+     * @throws HospitalException
+     */
     @Override
     public List<Doctor> getAll() throws HospitalException {
         return DaoFactory.doctorDao().getAll();
     }
 
+    /**
+     * Method that allows us to delete a doctor from the database
+     * @param id of the element we want to delete
+     * @throws HospitalException
+     */
     @Override
     public void delete(int id) throws HospitalException {
         DaoFactory.doctorDao().delete(id);
