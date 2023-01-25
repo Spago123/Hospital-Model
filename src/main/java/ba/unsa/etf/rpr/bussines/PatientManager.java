0ed0741b156d0cf06f5af059c3a8d316213b030c
Spago123 <hspago1@etf.unsa.ba>
2023-01-25 21:27:07 +0100

    JavaDoc PatientManager finished

--- src/main/java/ba/unsa/etf/rpr/bussines/PatientManager.java
+++ src/main/java/ba/unsa/etf/rpr/bussines/PatientManager.java
@@ -7,21 +7,45 @@ import ba.unsa.etf.rpr.exceptions.HospitalException;
 
 import java.util.List;
 
+/**
+ * PatientManager is a class that models our work with Patients table in the database
+ */
 public class PatientManager implements Manager<Patient>{
-
+    /**
+     * Method that lets us search the database for
+     * a patients with the given doctor
+     * @param doctor
+     * @return list of patients
+     * @throws HospitalException
+     */
     public List<Patient> getByDoctor(Doctor doctor) throws HospitalException {
         return DaoFactory.patientDao().searchByDoctor(doctor);
     }
 
+    /**
+     * Method that lets us get all patients from the database
+     * @return list of patients
+     * @throws HospitalException
+     */
     public List<Patient> getAll() throws HospitalException {
         return DaoFactory.patientDao().getAll();
     }
 
+    /**
+     * Method that deletes a patient from the database
+     * @param id of the element we want to delete
+     * @throws HospitalException
+     */
     @Override
     public void delete(int id) throws HospitalException {
      DaoFactory.patientDao().delete(id);
     }
 
+    /**
+     * Method that adds a patient to the database
+     * @param item we want to add
+     * @throws HospitalException
+     */
     @Override
     public void add(Patient item) throws HospitalException {
 
@@ -34,6 +58,11 @@ public class PatientManager implements Manager<Patient>{
         DaoFactory.patientDao().add(item);
     }
 
+    /**
+     * Method that lets us update a patient in the database
+     * @param item we want to update
+     * @throws HospitalException
+     */
     public void update(Patient item) throws HospitalException {
 
         if(!StuffManager.verifyPassword(item.getPassword()))
@@ -42,14 +71,33 @@ public class PatientManager implements Manager<Patient>{
         DaoFactory.patientDao().update(item);
     }
 
+    /**
+     * Method that lets us get a patient from the database by the id
+     * @param id we search
+     * @return
+     * @throws HospitalException
+     */
     public Patient getById(int id) throws HospitalException {
         return DaoFactory.patientDao().getById(id);
     }
 
+    /**
+     * Method that returns a patient that has the following parameters
+     * name and password
+     * @param name
+     * @param pass
+     * @return list of patients, should actually return only one patient
+     */
     public List<Patient> getByNameAndPass(String name, String pass){
         return DaoFactory.patientDao().searchByNameAndPass(name, pass);
     }
 
+    /**
+     * Method that lets us get a patient by name and UIN
+     * @param name
+     * @param UIN
+     * @return list of patients, should actually return one patient
+     */
     public List<Patient> getByNameAndUIN(String name, Long UIN){
         return DaoFactory.patientDao().searchByNameAndUin(name, UIN);
     }
